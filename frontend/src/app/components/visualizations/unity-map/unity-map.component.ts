9a6a50aaeb73937ea4dbec4574e1c4a6266bdb9c
RicoP <ricop@users.noreply.github.com>
2019-12-18 14:31:31 +0100

    call unity function

--- frontend/src/app/components/visualizations/unity-map/unity-map.component.ts
+++ frontend/src/app/components/visualizations/unity-map/unity-map.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import GeoJSON from 'ol/format/GeoJSON';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import { Layer } from 'ol/layer';
+
 import { Distribution } from '../../../../../../backend/src/shared/dataset';
+import { HttpProxyService } from './../../../http-proxy.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-unity-map',
@@ -11,7 +18,13 @@ export class UnityMapComponent implements OnInit {
 
   @Input() distribution: Distribution;
 
-  constructor() { }
+  constructor(
+    private proxy: HttpProxyService,
+    private snackBar: MatSnackBar
+  ) { 
+  }
+
+  public geojson: Layer;
 
   ngOnInit() {
   }
@@ -23,7 +36,27 @@ export class UnityMapComponent implements OnInit {
     //unity.parentNode = container;
     container.appendChild(unity);
 
-    //unityInstance.SendMessage('BrowserBindings', 'DigestJSON', 'MyString');
+
+    this.proxy.get(this.distribution.accessURL).subscribe(geojson => {
+      /*const vectorSource = new VectorSource({
+        features: (new GeoJSON()).readFeatures(res)
+      });
+      this.geojson = new VectorLayer({
+        source: vectorSource,
+        style: feature => this.styleFunction()
+      });
+      this.mapService.getMap(this.mapId).subscribe(map => {
+        const extent = vectorSource.getExtent();
+        map.getView().fit(extent);
+        this.createClickInteraction(map);
+      });*/
+
+      window.unityInstance.SendMessage('BrowserBindings', 'DigestJSON', JSON.stringify(geojson));
+    }, error => {
+      console.error(error);
+      const message = error.error && error.error.text ? error.error.text : JSON.stringify(error);
+      this.snackBar.open(message, null, { duration: 5000 });
+    });
   }
 
 }
